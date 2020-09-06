ae3420dd713d4982564b9cc374dd9aa89e3ace41
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-09-06 17:14:52 +0800

    Configure env variables

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module komentr-server
 
 go 1.14
 
-require github.com/labstack/echo/v4 v4.1.17
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/labstack/echo/v4 v4.1.17
+)
--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+  "os"
+  "fmt"
+  "log"
   "net/http"
   "github.com/labstack/echo/v4"
+  "github.com/joho/godotenv"
 )
 
 func main() {
+  if err := godotenv.Load(); err != nil {
+    log.Fatal("Error on loading .env file")
+  }
+
   e := echo.New()
 
   e.GET("/", func(c echo.Context) error {
     return c.String(http.StatusOK, "Hello")
   })
 
-  e.Logger.Fatal(e.Start("localhost:8080"))
+  port := os.Getenv("PORT")
+  url := fmt.Sprintf("localhost:%s", port)
+  e.Logger.Fatal(e.Start(url))
 }
