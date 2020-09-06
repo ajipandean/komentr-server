289cc39596ab52978c2889876f39e3b3290b7232
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-09-07 05:43:33 +0800

    Add before-save hooks in user model

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo/v4 v4.1.17
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 	gorm.io/driver/mysql v1.0.1
 	gorm.io/gorm v1.20.0
 )
--- models/user.go
+++ models/user.go
@@ -1,11 +1,29 @@
 package models
 
-import "komentr-server/helpers"
+import (
+  "strings"
+  "komentr-server/helpers"
+  "gorm.io/gorm"
+  b "golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
   helpers.Model
-  Username string `json:"username" gorm:"type:varchar(70)"`
+  Username string `json:"username" gorm:"type:varchar(70);"`
   Email string `json:"email" gorm:"type:varchar(100);not null;unique"`
   Password string `json:"password" gorm:"not null;type:varchar(100)"`
   Comments []Comment `json:"comments"`
+}
+
+func (u *User) BeforeSave(tx *gorm.DB) error {
+  bytePassword := []byte(u.Password)
+  hashedPassword, err := b.GenerateFromPassword(bytePassword, b.DefaultCost)
+  if err != nil {
+    panic(err)
+  }
+  u.Password = string(hashedPassword)
+
+  splittedEmail := strings.Split(u.Email, "@")
+  u.Username = splittedEmail[0]
+  return nil
 }
