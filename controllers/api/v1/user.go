97ed52a43fcdf607447e49949c0e1508b83e661e
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-09-08 06:11:04 +0800

    Parse logged in user data and pass it to fetch user with comment

--- controllers/api/v1/user.go
+++ controllers/api/v1/user.go
@@ -1,6 +1,7 @@
 package controllers_v1
 
 import (
+  "fmt"
   "net/http"
   "github.com/labstack/echo/v4"
   "komentr-server/models"
@@ -10,13 +11,15 @@ import (
 
 func GetUser(c echo.Context) error {
   u := h.GetLoggedinUser(c)
-  return c.JSON(http.StatusOK, u["username"])
+  return c.JSON(http.StatusOK, u)
 }
 
 func GetUserComments(c echo.Context) error {
   user := new(models.User)
-  if err := s.FindUserWithComments(2, user); err != nil {
-    panic(err)
+  u := h.GetLoggedinUser(c)
+  id := uint(u["id"].(float64))
+  if err := s.FindUserWithComments(id, user); err != nil {
+    fmt.Println(err.Error())
   }
   return c.JSON(http.StatusOK, user)
 }
