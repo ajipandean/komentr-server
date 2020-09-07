76a47e03af05bb07c794ccac6db96e49baeaf24d
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-09-08 06:38:05 +0800

    Add create-comment based on logged in user

--- controllers/api/v1/comment.go
+++ controllers/api/v1/comment.go
@@ -4,14 +4,17 @@ import (
   "net/http"
   "github.com/labstack/echo/v4"
   "komentr-server/models"
+  h "komentr-server/helpers"
   s "komentr-server/services/api/v1"
 )
 
 func StoreComment(c echo.Context) error {
   comment := new(models.Comment)
+  u := h.GetLoggedinUser(c)
   if err := c.Bind(comment); err != nil {
     panic(err)
   }
+  comment.UserID = uint(u["id"].(float64))
   if err := s.CreateComment(comment); err != nil {
     panic(err)
   }
